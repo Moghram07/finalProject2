61dd6d1c0df6b4a150ff83db6013ed47cd5bc104
OMAR ALSHEHRI <166976917+Moghram07@users.noreply.github.com>
2024-09-13 18:16:12 +0300

    Add files via upload

---
+++ src/main/java/com/example/finalproject/Model/Document.java
@@ -0,0 +1,27 @@
+package com.example.finalproject.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    private String title;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")// like document to course
+    private Course course;
+}
+
